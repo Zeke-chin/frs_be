d31a4d90ed70b310d2e192584eeecbe0f178dc7b
xjc299 <2504550737@qq.com>
2023-01-14 13:34:18 +0800

    routers里hello.py和schemas里init.py注释

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 # Dependency
 def get_db():
     try:
-        from app.models.database import SessionLocal
+        from app.models.database import SessionLocal#从app.models.database中导入SessionLocal。
         db = SessionLocal()
         yield db
     finally:
-        db.close()
+        db.close()## 用get_db()来用于获取sessionLocal以及及时关闭连接
 
-async def get_page(
-    page: int = 1, size: int = 10
+async def get_page(#定义页数
+    page: int = 1, size: int = 10#当前为第一页，每页的记录数,默认为10。
 ):
     return {"page": page, "size": size}
 
--- app/routers/hello.py
+++ app/routers/hello.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends
-from fastapi_pagination import paginate, Params
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends#用户相关的路径操作与其余代码分开
+from fastapi_pagination import paginate, Params#导入fastapi_pagination，用于将数据进行分页和提供分页参数
+from sqlalchemy.orm import Session#导入orm
 
-from app import schemas, get_db, crud
-from utils import web_try, sxtimeit
+from app import schemas, get_db, crud#在app建包：创建模型，也就是接口传的一些参数；get_db函数；放置执行数据操作
+from utils import web_try, sxtimeit#在utils设web_try, sxtimeit
 
 router_hello = APIRouter(
-    prefix="/hello",
-    tags=["hello-测试"],
+    prefix="/hello",#路由的前缀
+    tags=["hello-测试"],#将应用于特定路径操作的内容
 )
 
 
